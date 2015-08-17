b9d810c6ce3bbfe0b8304c10a69d3380ea369b1c
Kirill A. Kornilov <kirill@tp>
2015-08-17 17:01:41 +0300

    update build script

--- build_rel.sh
+++ build_rel.sh
@@ -12,12 +12,15 @@ make clean
 make
 mv build-atmega328/osd.hex ../$rel/MinimOSDx2_en-$ver.hex
 cd ..
+cd config
+sh build.sh
+cd ..
 cp config/minimosd_cfg $rel/
 cp config/*.mcm $rel/
 cp doc/*.pdf $rel/
 cd $rel
-cp minimosd_cfg minimosd_cfg.tcl
-../../../freewrap/linux64/freewrapTCLSH -w ../../../freewrap/win32/freewrapTCLSH.exe minimosd_cfg.tcl
-rm minimosd_cfg.tcl
+cp minimosd_cfg _minimosd_cfg.tcl
+../../../freewrap/linux64/freewrapTCLSH -w ../../../freewrap/win32/freewrapTCLSH.exe _minimosd_cfg.tcl
+rm _minimosd_cfg.tcl
 cd ..
 zip -r MinimOSDx2-$ver.zip MinimOSDx2-$ver
