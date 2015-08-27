9ce499e5fb0305c8450b7a42b626ea5a0894a3aa
Kirill A. Kornilov <kirill@tp>
2015-08-27 18:12:59 +0300

    build script update

--- build_rel.sh
+++ build_rel.sh
@@ -19,8 +19,8 @@ cp config/minimosd_cfg $rel/
 cp config/*.mcm $rel/
 cp doc/*.pdf $rel/
 cd $rel
-cp minimosd_cfg _minimosd_cfg.tcl
-../../../freewrap/linux64/freewrapTCLSH -w ../../../freewrap/win32/freewrapTCLSH.exe _minimosd_cfg.tcl
-rm _minimosd_cfg.tcl
+cp minimosd_cfg minimosd_cfg.tcl
+../../../freewrap/linux64/freewrapTCLSH -w ../../../freewrap/win32/freewrapTCLSH.exe minimosd_cfg.tcl
+rm minimosd_cfg.tcl
 cd ..
 zip -r MinimOSDx2-$ver.zip MinimOSDx2-$ver
