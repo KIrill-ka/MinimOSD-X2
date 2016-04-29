02c8e45b182f85f34631343afa2d997413ad55e2
Kirill A. Kornilov <kirill@tp>
2016-04-29 14:59:47 +0300

    dual lang font

--- osd/strings_ru_utf.h
+++ osd/strings_ru_utf.h
@@ -1,13 +1,13 @@
 #pragma once
-#define TXT_VERSION "Миним Х2 вер.%d.%d.%d|Самолёт|"
-#define TXT_VERSION_FONT "Версия шрифта: "
-#define TXT_WARN_NOGPS     "НЕТ ПОЗИЦИИ!"
-#define TXT_WARN_STALL     " СКОР.МАЛА!"
-#define TXT_WARN_OVERSPEED "ПРЕВ.СКОРОСТ"
-#define TXT_WARN_LOWBATT   "НИЗК.ЗАРЯД!"
-#define TXT_WARN_RSSILOW   "ПЛОХ.СИГНАЛ!"
-#define TXT_WARN_MOTOR     "МОТОР ОТКЛ.!"
-#define TXT_WARN_NOMAV     "НЕТ ДАННЫХ!"
+#define TXT_VERSION "миним х2 вер.%d.%d.%d|самолёт|"
+#define TXT_VERSION_FONT "версия шрифта: "
+#define TXT_WARN_NOGPS     "нет позиции!"
+#define TXT_WARN_STALL     " скор.мала!"
+#define TXT_WARN_OVERSPEED "прев.скорост"
+#define TXT_WARN_LOWBATT   "низк.заряд!"
+#define TXT_WARN_RSSILOW   "плох.сигнал!"
+#define TXT_WARN_MOTOR     "мотор откл.!"
+#define TXT_WARN_NOMAV     "нет данных!"
 
 #define TXT_MODE_MANUAL "ручн"
 #define TXT_MODE_CIRCLE "круг"
@@ -24,7 +24,7 @@
 #define TXT_MODE_GUIDED "мест"
 #define TXT_MODE_INITIALIZING "подг"
 
-#define CH_WP1 'П'
+#define CH_WP1 'п'
 #define CH_WP2 'т'
-#define CH_XTRACK_ERROR1 'О'
+#define CH_XTRACK_ERROR1 'о'
 #define CH_XTRACK_ERROR2 'ш'
